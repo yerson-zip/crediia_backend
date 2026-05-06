bf395c7022fe6a94effddd393e6db952a80d68c8
yerson.zip <yersonochoa.2005@gmail.com>
2026-05-06 02:26:59 -0500

    v3

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://crediia-front.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
