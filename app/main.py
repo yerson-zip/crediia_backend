6040b293afed28de1dabf3a6d44154cc0e3bf3b9
yerson.zip <yersonochoa.2005@gmail.com>
2026-05-06 02:30:53 -0500

    v4

--- app/main.py
+++ app/main.py
@@ -4,9 +4,13 @@ from app.router import regresion, red_neuronal
 
 app = FastAPI()
 
+origins = [
+    "https://crediia-front.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://crediia-front.vercel.app/"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
